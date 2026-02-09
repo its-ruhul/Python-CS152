52bb90cd49a700835917f5e46e38f5d9dc543f5d
Ruhul <itsruhul@proton.me>
2026-02-09 14:29:49 +0530

    completed day 1 classwork

---
+++ day1/day1-2.py
@@ -0,0 +1,5 @@
+a = 4
+b = 5
+
+ans = int((5 * a + a * b ** 2) / ((a ** 2 + 9) ** (1/2)))
+print("The answer is:", ans)
---
+++ day1/day1.py
@@ -0,0 +1,1 @@
+print('"Hello World" \n % Hello World % \n \t \\\\ Hello World \\\\')
