6bf6523c630b650dd07e93b8ba39995e5d2c57d0
Ruhul <itsruhul@proton.me>
2026-02-09 14:37:00 +0530

    started with GkG but changed my mind now

---
+++ day1/gkgLearnPythonBasics.py
@@ -0,0 +1,62 @@
+print("Hello Geeks, Welcome to Python Basics")
+
+#-----------------------------------------------#
+
+# This is a single-line comment
+
+"""
+This is a
+multi-line comment
+or docstring.
+"""
+
+#-----------------------------------------------#
+
+# Integer assignment
+a = 45
+
+# Floating-point assignment
+b = 1456.8
+
+# String assignment
+c = "Geek"
+
+print(a)    
+print(b) 
+print(c)
+
+#-----------------------------------------------#
+
+x = "Hello World" # string
+x = 50  # integer
+x = 60.5  # float
+x = 3j  # complex
+x = ["geeks", "for", "geeks"]  # list 
+x = ("geeks", "for", "geeks")  # tuple
+x = {"name": "Suraj", "age": 24} # dict
+x = {"geeks", "for", "geeks"} # set
+x = True  # bool
+x = b"Geeks" # binary
+print(x)
+
+#-----------------------------------------------#
+
+
+
+#-----------------------------------------------#
+
+
+
+#-----------------------------------------------#
+
+
+
+#-----------------------------------------------#
+
+
+
+#-----------------------------------------------#
+
+
+
+#-----------------------------------------------#
